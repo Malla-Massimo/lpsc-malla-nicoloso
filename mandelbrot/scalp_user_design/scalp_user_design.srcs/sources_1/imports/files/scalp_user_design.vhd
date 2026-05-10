b75a095a8e5ecdf71bdd2503248f5404b428abd4
Massimo Malla <mallam99@hotmail.com>
2026-05-10 13:09:47 +0200

    WIP: multi cores

--- mandelbrot/scalp_user_design/scalp_user_design.srcs/sources_1/imports/files/scalp_user_design.vhd
+++ mandelbrot/scalp_user_design/scalp_user_design.srcs/sources_1/imports/files/scalp_user_design.vhd
@@ -595,10 +595,7 @@ begin
         signal ram_we        : std_logic_vector(0 downto 0)  := (others => '0');
         signal write_done : std_logic := '0';
         
-        signal julia_start : std_logic := '0';
-        signal julia_x_coord : sfixed(3 downto -15); 
-        signal julia_y_coord : sfixed(3 downto -15);
-        signal julia_n_iter : std_logic_vector(7 downto 0);
+       
         signal julia_done: std_logic;
         signal julia_x_step : sfixed(3 downto -15) := to_sfixed(0.0041666, 3, -15);
         signal julia_y_step : sfixed(3 downto -15) := to_sfixed(0.0041666, 3, -15);
@@ -606,19 +603,38 @@ begin
         signal cur_y_int: unsigned (9 downto 0);
         signal julia_range : sfixed(3 downto -15) := to_sfixed(3.0, 3, -15);
         signal x_initial_left : sfixed(3 downto -15);
-        signal addr_counter : unsigned(18 downto 0);
-        signal palette_index : std_logic_vector(4 downto 0);
+        
         type state_t is (INIT_RANGE, INIT_STEP, INIT_COORD, CALCULATE, WAIT_FOR_ACK, DONE);
         signal palette_index_reg : std_logic_vector(4 downto 0);
         signal state: state_t := INIT_RANGE;
 
         -- Julia Pipelined
-        signal addr_done: unsigned(18 downto 0);
-        signal julia_busy: std_logic;
         signal v_sync_trigger: std_logic := '0';
         signal v_sync_reg : std_logic_vector(2 downto 0) := "000";
         signal v_sync_occurred:  std_logic := '0';
         signal VidOn_delayed : std_logic := '0';
+        
+        signal cores_done: std_logic_vector(1 downto 0) := "00";
+
+        -- Core 0
+        signal julia_start : std_logic := '0';
+        signal julia_x_coord : sfixed(3 downto -15); 
+        signal julia_y_coord : sfixed(3 downto -15);
+        signal addr_counter : unsigned(18 downto 0);
+        signal julia_n_iter : std_logic_vector(7 downto 0);
+        signal palette_index : std_logic_vector(4 downto 0);
+        signal addr_done: unsigned(18 downto 0);
+        signal julia_busy: std_logic;
+
+        -- Core 1
+        signal julia_start_1 : std_logic := '0';
+        signal julia_x_coord_1: sfixed(3 downto -15); 
+        signal julia_y_coord_1: sfixed(3 downto -15); 
+        signal addr_counter_1: unsigned(18 downto 0);
+        signal julia_n_iter_1 : std_logic_vector(7 downto 0);
+        signal palette_index_1 : std_logic_vector(4 downto 0);
+        signal addr_done_1: unsigned(18 downto 0);
+        signal julia_busy_1: std_logic;
 
     component BRAM_5_500k is
       PORT (
@@ -789,6 +805,12 @@ begin
         iteration => julia_n_iter,
         palette_index => palette_index
     );
+
+    color_index_1: color_palette_index
+    port map(
+        iteration => julia_n_iter_1,
+        palette_index => palette_index_1
+    );
     
     julia_pipelined: JuliaPipelined
     port map (
@@ -807,6 +829,25 @@ begin
         done        => julia_done,
         addr_done   => addr_done,
         julia_busy  => julia_busy
+    );
+
+    julia_pipelined_1: JuliaPipelined
+    port map (
+        clk     => clk_100MHz,
+        rst     => '0',
+        start   =>  julia_start_1,
+
+        x       => julia_x_coord_1,
+        y       => julia_y_coord_1,
+        ram_addr => addr_counter_1,
+
+        c_re    => to_sfixed(-0.835, 3, -15), -- Julia Real constant
+        c_im    => to_sfixed(-0.232, 3, -15), -- Julia Imaginary constant
+
+        n_iteration => julia_n_iter_1,
+        done        => julia_done_1,
+        addr_done   => addr_done_1,
+        julia_busy  => julia_busy_1
     );
 
 --    Aurora : aurora_8b10b
@@ -1208,8 +1249,9 @@ begin
                     WRCLK  => ClpxNumRegsAxixD.ClockxC.ClkxC,
                     WREN   => '1');
                 
-             ---------------------------------------------------------------------------
-            -- JULIA PROCESS PIPELINED
+
+            ---------------------------------------------------------------------------
+            -- JULIA PROCESS PIPELINED MULTIPROCESS
             ---------------------------------------------------------------------------        
             JuliaPipe: process(clk_100MHz)
             begin
@@ -1243,6 +1285,10 @@ begin
                                 -- Now steps are updated, we can calculate initial coordinates
                                 julia_x_coord <= resize(to_sfixed(-360, 10, 0) * julia_x_step, 3, -15);
                                 julia_y_coord <= resize(to_sfixed(-360, 10, 0) * julia_y_step, 3, -15);
+
+                                julia_x_coord_1 <= resize(to_sfixed(-360, 10, 0) * julia_x_step, 3, -15);
+                                julia_y_coord_1 <= resize(to_sfixed(-1, 10, 0) * julia_y_step, 3, -15);
+
                                 x_initial_left <= resize(to_sfixed(-360, 10, 0) * julia_x_step, 3, -15);
 
                                 -- julia_start <= '1';
@@ -1255,6 +1301,11 @@ begin
                                     ram_we      <= "1";
                                     ram_wr_addr <= std_logic_vector(addr_done);
                                     ram_data_in <= palette_index;
+
+                                elsif julia_done_1 = '1' then
+                                    ram_we      <= "1";
+                                    ram_wr_addr <= std_logic_vector(addr_done_1);
+                                    ram_data_in <= palette_index_1;
                                 end if;
 
                                 -- 2. Feed new pixels safely
@@ -1272,21 +1323,52 @@ begin
                                             julia_x_coord <= resize(julia_x_coord + julia_x_step, 3, -15);
                                             addr_counter  <= addr_counter + 1;
                                         else
-                                            if cur_y_int < 719 then
+                                            if cur_y_int < 359 then
                                                 cur_x_int     <= (others => '0');
                                                 cur_y_int     <= cur_y_int + 1;
                                                 julia_x_coord <= x_initial_left;
                                                 julia_y_coord <= resize(julia_y_coord + julia_y_step, 3, -15);
                                                 addr_counter  <= addr_counter + 1;
                                             else
-                                                state <= DONE;
+                                                cores_done(0) <= "1";
+                                            end if;
+                                        end if;
+                                    end if;
+
+                                -- 2. Feed new pixels safely
+                                if julia_busy_1 = '0' then
+                                    
+                                    if julia_start_1 = '0' then
+                                        -- PHASE 1: Assert start. 
+                                        julia_start_1 <= '1';
+                                        
+                                    else
+                                        julia_start_1 <= '0';
+                                        
+                                        if cur_x_int < 719 then
+                                            cur_x_int     <= cur_x_int + 1;
+                                            julia_x_coord_1 <= resize(julia_x_coord + julia_x_step, 3, -15);
+                                            addr_counter_1  <= addr_counter_1 + 1;
+                                        else
+                                            if cur_y_int < 719 then
+                                                cur_x_int     <= (others => '0');
+                                                cur_y_int     <= cur_y_int + 1;
+                                                julia_x_coord_1 <= x_initial_left;
+                                                julia_y_coord_1 <= resize(julia_y_coord_1 + julia_y_step, 3, -15);
+                                                addr_counter_1  <= addr_counter_1 + 1;
+                                            else
+                                                cores_done(1) <= "1";
                                             end if;
                                         end if;
                                     end if;
                                     
                                 else
                                     -- Ensure start remains low while busy so we don't double-feed
-                                    julia_start <= '0';
+                                    julia_start_1 <= '0';
+                                end if;
+
+                                if cores_done(1) == '1' and cores_done(0) == '0' then
+                                    state <= DONE
                                 end if;
                                                         
                             when DONE =>
@@ -1297,8 +1379,98 @@ begin
                     end if;
                 end if;
             end process JuliaPipe;
+
             
-           
+            ---------------------------------------------------------------------------
+            -- JULIA PROCESS PIPELINED
+            ---------------------------------------------------------------------------        
+            -- JuliaPipe: process(clk_100MHz)
+            -- begin
+            --     if rising_edge(clk_100MHz) then
+
+            --         if Clk125PllLockedxS = '1' then
+            --             case state is 
+            --                when INIT_RANGE => 
+            --                     cur_x_int <= (others => '0');
+            --                     cur_y_int <= (others => '0');
+            --                     addr_counter <= (others => '0');
+            --                     ram_we <= "0";
+
+            --                     if julia_range > to_sfixed(0.1, 3, -15) then
+            --                         julia_range <= resize(julia_range - to_sfixed(0.005, 3, -15), julia_range);
+            --                     else
+            --                         julia_range <= to_sfixed(3.0, 3, -15);
+            --                     end if;
+
+            --                     if v_sync_occurred = '1' then
+            --                         state <= INIT_STEP;
+            --                     end if;
+
+            --                 when INIT_STEP =>
+            --                     -- Now julia_range is updated, we can calculate steps
+            --                     julia_x_step <= resize(julia_range * to_sfixed(0.001388, 0, -15), 3, -15);
+            --                     julia_y_step <= resize(julia_range * to_sfixed(0.001388, 0, -15), 3, -15);
+            --                     state <= INIT_COORD; -- Wait for steps to update
+
+            --                 when INIT_COORD =>
+            --                     -- Now steps are updated, we can calculate initial coordinates
+            --                     julia_x_coord <= resize(to_sfixed(-360, 10, 0) * julia_x_step, 3, -15);
+            --                     julia_y_coord <= resize(to_sfixed(-360, 10, 0) * julia_y_step, 3, -15);
+            --                     x_initial_left <= resize(to_sfixed(-360, 10, 0) * julia_x_step, 3, -15);
+
+            --                     -- julia_start <= '1';
+            --                     state <= CALCULATE;
+                                
+            --                when CALCULATE =>
+            --                     ram_we <= "0";
+
+            --                     if julia_done = '1' then
+            --                         ram_we      <= "1";
+            --                         ram_wr_addr <= std_logic_vector(addr_done);
+            --                         ram_data_in <= palette_index;
+            --                     end if;
+
+            --                     -- 2. Feed new pixels safely
+            --                     if julia_busy = '0' then
+                                    
+            --                         if julia_start = '0' then
+            --                             -- PHASE 1: Assert start. 
+            --                             julia_start <= '1';
+                                        
+            --                         else
+            --                             julia_start <= '0';
+                                        
+            --                             if cur_x_int < 719 then
+            --                                 cur_x_int     <= cur_x_int + 1;
+            --                                 julia_x_coord <= resize(julia_x_coord + julia_x_step, 3, -15);
+            --                                 addr_counter  <= addr_counter + 1;
+            --                             else
+            --                                 if cur_y_int < 719 then
+            --                                     cur_x_int     <= (others => '0');
+            --                                     cur_y_int     <= cur_y_int + 1;
+            --                                     julia_x_coord <= x_initial_left;
+            --                                     julia_y_coord <= resize(julia_y_coord + julia_y_step, 3, -15);
+            --                                     addr_counter  <= addr_counter + 1;
+            --                                 else
+            --                                     state <= DONE;
+            --                                 end if;
+            --                             end if;
+            --                         end if;
+                                    
+            --                     else
+            --                         -- Ensure start remains low while busy so we don't double-feed
+            --                         julia_start <= '0';
+            --                     end if;
+                                                        
+            --                 when DONE =>
+            --                     state <= INIT_RANGE;
+            --                 when others => 
+            --                     state <= INIT_RANGE;
+            --             end case;
+            --         end if;
+            --     end if;
+            -- end process JuliaPipe;
+
 
             ---------------------------------------------------------------------------
             -- JULIA PROCESS 
